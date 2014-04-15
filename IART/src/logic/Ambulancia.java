160636ccba15f683facc0c066979aa1b6348acaf
mspkvp <msp.univ.por@gmail.com>
2014-04-15 18:31:51 +0100

    some classes created

---
+++ IART/src/logic/Ambulancia.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class Ambulancia {
+	final int capacidade;
+	private int nrOcupantes;
+	
+	public Ambulancia(int capacidade){
+		this.capacidade = capacidade;
+		nrOcupantes = 0;
+	}
+}
---
+++ IART/src/logic/Sucursal.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class Sucursal {
+	final private int capacidade_pacientes;
+	
+	public Sucursal(int capacidade){
+		capacidade_pacientes = capacidade;
+	}
+}
