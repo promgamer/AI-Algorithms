98a205c0996bf2da8e23fec53673cf0635bbb951
mspkvp <msp.univ.por@gmail.com>
2014-04-15 20:09:44 +0100

    logica almost done?

--- IART/src/logic/Ambulancia.java
+++ IART/src/logic/Ambulancia.java
@@ -1,11 +1,36 @@
 package logic;
 
 public class Ambulancia {
-	final int capacidade;
+	final static int combustivel_max = 80; // 80 litros
+	final static float consumo_km = 0.08f; // 8 l / 100 km
+	final int capacidade_ocupantes;
 	private int nrOcupantes;
+	private float combustivel;
 	
 	public Ambulancia(int capacidade){
-		this.capacidade = capacidade;
+		capacidade_ocupantes = capacidade;
 		nrOcupantes = 0;
+		combustivel = combustivel_max;
+	}
+	
+	public void abastecer(){
+		combustivel = combustivel_max;
+	}
+	
+	public void consumir(int km){
+		combustivel -= km*consumo_km;
+	}
+	
+	public float combustivel_restante(){
+		return combustivel;
+	}
+	
+	public int ocupar(int nr){
+		if( nrOcupantes + nr > capacidade_ocupantes)
+			return -1;
+		else{
+			nrOcupantes += nr;
+			return nrOcupantes;
+		}
 	}
 }
--- IART/src/logic/Sucursal.java
+++ IART/src/logic/Sucursal.java
@@ -2,8 +2,34 @@ package logic;
 
 public class Sucursal {
 	final private int capacidade_pacientes;
+	private int nrPacientes;
 	
 	public Sucursal(int capacidade){
 		capacidade_pacientes = capacidade;
+		nrPacientes = 0;
+
+	}
+	
+	public int encher(int nr){
+		if( nrPacientes + nr > capacidade_pacientes)
+			return -1;
+		else{
+			nrPacientes += nr;
+			return nrPacientes;
+		}
+	}
+	
+	public void retirar(int nr){
+		nrPacientes -= nr;
+	}
+	
+	public int esvaziar(){
+		int res = nrPacientes;
+		nrPacientes = 0;
+		return res;
+	}
+	
+	public int pacientes_presentes(){
+		return nrPacientes;
 	}
 }
