da8feae42089f0be3dfebaa54442d561281fafe9
mspkvp <msp.univ.por@gmail.com>
2014-05-24 20:28:08 +0100

    ....

--- IART/src/simulated/annealing/SimulatedAnnealing.java
+++ IART/src/simulated/annealing/SimulatedAnnealing.java
@@ -1,14 +1,18 @@
 package simulated.annealing;
 
+import logic.Clinica;
+
 public class SimulatedAnnealing {
 	private double temperaturaAtual = 0, temperaturaFinal = 0;
 	private double taxaArrefecimento = 0;
+	private Clinica clinica = null;
 	
     
-	public SimulatedAnnealing(double temperaturaInicial, double taxaArrefecimento, double temperaturaFinal){
+	public SimulatedAnnealing(double temperaturaInicial, double taxaArrefecimento, double temperaturaFinal, Clinica clinica){
 		this.temperaturaAtual = temperaturaInicial;
 		this.taxaArrefecimento = taxaArrefecimento;
 		this.temperaturaFinal = temperaturaFinal;
+		this.clinica = clinica;
 	}
 	
 	private double probabilidadeEscolha(int energia, int energiaProxSolucao, double temperatura) {
