de1d5d72511afd51f51e9e4ad4db542a587f647f
mspkvp <msp.univ.por@gmail.com>
2014-05-28 15:27:38 +0100

    debugging stuff

--- IART/src/logic/Ambulancia.java
+++ IART/src/logic/Ambulancia.java
@@ -1,7 +1,7 @@
 package logic;
 
 public class Ambulancia {
-	final static double combustivel_max = 4;
+	final static double combustivel_max = 400;
 	final int capacidade_ocupantes;
 	private int nrOcupantes;
 	private double combustivel;
